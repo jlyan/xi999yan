97b476793eb629036e95e6cc66db2c1bebac9aa2
jlyan <jinlai.yan@gmail.com>
2011-11-15 11:08:04 +0800

    Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xi999yan::Application.config.secret_token = '3c431f788138fd94fec88327109d256c5d441eaec2ced75c84dc2d6d2192314744df02dcabe9c958229af391a503b8665acf8c77fde88c5ea25af9ecd8670214'
